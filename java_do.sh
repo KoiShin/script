d07fcb5e344258c480620a9ede1e9b4487773d14
KoiShin <koikeshinnosuke0415@gmail.com>
2015-11-18 16:51:51 +0900

    don't show message when it compiles java file

--- java_do.sh
+++ java_do.sh
@@ -2,12 +2,12 @@
 
 if [ "$1" = "" ]; then
     echo "No argument!!"
-    echo "Please enter some argument"
+    echo "Please enter some argument!!"
     exit
 fi
 
 FILENAME=${1%.*}
 
-echo -e "Compiling...\n"
+# echo -e "Compiling...\n"
 javac ${FILENAME}.java &&
 java ${FILENAME}
